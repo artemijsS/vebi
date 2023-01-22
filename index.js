9626cb92b7b2bc6d10cd37a90287a79862d0ce62
artemijsS <artjom1408@gmail.com>
2023-01-22 18:53:47 +0200

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({ extended: true }));
+
+
+const PORT = process.env.PORT || 5000;
+
+
+//**************************
+//  REACT APP
+//**************************
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+//**************************
+
+app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
