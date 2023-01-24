d2151e61c26879c082d6796299e0ed72c1249f29
artemijsS <artjom1408@gmail.com>
2023-01-24 23:48:31 +0200

    CI/CD aws

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source ~/.nvm/nvm.sh
 cd /home/ec2-user/vebi
-/home/ec2-user/.nvm/versions/node/v16.19.0/bin/npm install
-/home/ec2-user/.nvm/versions/node/v16.19.0/bin/npm run build
+echo "$PATH"
+npm install
+npm run build
