0eb956d8e8547ab0bf8154be65ae04394068b563
artemijsS <artjom1408@gmail.com>
2023-01-25 00:38:29 +0200

    CI/CD aws

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -2,7 +2,7 @@
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-export HOME="/home/ubuntu/"
+export HOME="/home/ec2-user/"
 cd /home/ec2-user/vebi
 whereis npm
 npm install
