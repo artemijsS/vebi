f47c6d4c9925c980106cfda4c5aa99fbbc892d12
artemijsS <artjom1408@gmail.com>
2023-01-25 02:09:00 +0200

    CI/CD aws

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#source /home/ec2-user/.bash_profile
-source ~root/.bash_profile
+source /home/ec2-user/.bash_profile
+sudo source ~root/.bash_profile
 cd /home/ec2-user/vebi
 sudo npm install
 sudo npm run build
