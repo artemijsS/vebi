d3d47087ae75a34cf4bfc5a39137644f7b631f50
artemijsS <artjom1408@gmail.com>
2023-01-25 02:22:58 +0200

    CI/CD aws

--- scripts/install_app_dependencies.sh
+++ scripts/install_app_dependencies.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-source /home/ec2-user/.bash_profile
-sudo source ~root/.bash_profile
+#source /home/ec2-user/.bash_profile
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 cd /home/ec2-user/vebi
 sudo npm install
 sudo npm run build
