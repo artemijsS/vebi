890c9eb739e6ee23034f4c2deefbde759db831ab
artemijsS <artjom1408@gmail.com>
2023-02-08 13:05:35 +0200

    RedirectPage

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { MainPage } from './pages';
+import { MainPage, RedirectPage } from './pages';
 
 export const useRoutes = ():JSX.Element => {
 
     return (
         <Routes>
             <Route path={"/"} element={ <MainPage/> }/>
+            <Route path={"/facebook"} element={ <RedirectPage page={"https://www.facebook.com/people/VEBI/100089967131850"}/> }/>
+            <Route path={"/google"} element={ <RedirectPage page={"https://www.google.com/search?q=VEBI"}/> }/>
             <Route path={"*"} element={ <Navigate to={"/"}/> }/>
         </Routes>
     )
