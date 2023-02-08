4b499b43bb7e00776c1589ccc0566573cd32e7e8
artemijsS <artjom1408@gmail.com>
2023-02-08 13:15:00 +0200

    RedirectPage && start script pm2 change

---
+++ client/src/pages/RedirectPage.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import { Helmet } from 'react-helmet';
+
+interface RedirectPageProps {
+    page: string
+}
+
+function RedirectPage({ page }: RedirectPageProps): JSX.Element {
+
+    useEffect(() => {
+        window.location.replace(page);
+    }, [])
+
+    return (
+        <div>
+            <Helmet>
+                <title>VEBI</title>
+            </Helmet>
+        </div>
+    )
+}
+
+export default RedirectPage;
