c87f4cf9f966d152bb235324d19712274395523f
Artemijs Semjaņņikovs <76394476+artemijsS@users.noreply.github.com>
2023-02-08 13:15:52 +0200

    RedirectPage && start script pm2 change (#5)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { UnderConstructionPage } from './pages';
+import { UnderConstructionPage, RedirectPage } from './pages';
 import setCssProperties from './utils/cssProperties'
 
 setCssProperties();
@@ -10,6 +10,8 @@ function App() {
       <BrowserRouter>
           <Routes>
             <Route path={"/"} element={ <UnderConstructionPage/> }/>
+            <Route path={"/facebook"} element={ <RedirectPage page={"https://www.facebook.com/people/VEBI/100089967131850"}/> }/>
+            <Route path={"/google"} element={ <RedirectPage page={"https://www.google.com/search?q=VEBI"}/> }/>
             <Route path={"*"} element={ <Navigate to={"/"}/> }/>
           </Routes>
       </BrowserRouter>
