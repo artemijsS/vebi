1bbdaaad95cb53b12ce2797eb7250d72c36b764d
artemijsS <artjom1408@gmail.com>
2023-03-22 12:56:51 +0200

    facebook redirect to new url

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
     return (
         <Routes>
             <Route path={"/"} element={ <MainPage/> }/>
-            <Route path={"/facebook"} element={ <RedirectPage page={"https://www.facebook.com/people/VEBI/100089967131850"}/> }/>
+            <Route path={"/facebook"} element={ <RedirectPage page={"https://www.facebook.com/vebi.facebook"}/> }/>
             <Route path={"/google"} element={ <RedirectPage page={"https://www.google.com/search?q=VEBI"}/> }/>
             <Route path={"*"} element={ <RedirectPage page={"https://vebi.lv/"}/> }/>
         </Routes>
