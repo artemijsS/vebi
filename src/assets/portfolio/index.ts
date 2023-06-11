f3440ba0c495f84fb60d8cc7afdd6e739a9f294a
artemijsS <artjom1408@gmail.com>
2023-06-11 19:55:14 +0300

    portfolio update

--- src/assets/portfolio/index.ts
+++ src/assets/portfolio/index.ts
@@ -1,3 +1,8 @@
 export { default as EnergyPlus } from "./energyplus.png"
 export { default as Fam3Flyer } from "./fam3flyer.png"
 export { default as EnergyPlusFlyer } from "./energyplusFlyer.png"
+export { default as Paphos } from "./paphos.jpg"
+export { default as Sweden } from "./sweden.jpg"
+export { default as MaksV } from "./maksV.jpg"
+export { default as Hashtify } from "./hashtify.jpg"
+export { default as Adventus } from "./adventus.jpg"
